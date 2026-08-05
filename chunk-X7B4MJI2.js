3042b26ea7ee4128a31e08787a111bd45a55e24e
Cocoa-Puffs <31016907+Cocoa-Puffs@users.noreply.github.com>
2026-08-05 09:47:51 +0000

    Deploying to gh-pages from @ Cocoa-Puffs/web-app@e842bbab25ebeee1653ec4428449c840e08f1dbb 🚀

---
+++ chunk-X7B4MJI2.js
@@ -0,0 +1,1 @@
+import{Oa as r}from"./chunk-6VMDU47F.js";import{ea as c,ja as e}from"./chunk-CL3CE66M.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getGlAccounts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
